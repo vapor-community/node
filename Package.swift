462cb39190b9da5b84ca88b32c60fa52a3247a11
Tanner Nelson <me@tanner.xyz>
2016-07-22 12:57:55 -0400

    path indexable ++

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-      .Package(url: "https://github.com/qutheory/path-indexable.git", majorVersion: 0, minor: 2),
+      .Package(url: "https://github.com/qutheory/path-indexable.git", majorVersion: 0, minor: 3),
       .Package(url: "https://github.com/qutheory/polymorphic.git", majorVersion: 0, minor: 3)
     ],
     exclude: [
