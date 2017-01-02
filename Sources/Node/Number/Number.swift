6f8604a7d073c30479de65e21c94b095b8c467ff
Evert van Brussel <ecvanbrussel@gmail.com>
2017-01-02 14:02:29 +0100

    Added support for Decimal

--- Sources/Node/Number/Number.swift
+++ Sources/Node/Number/Number.swift
@@ -1,8 +1,11 @@
+import Foundation
+
 extension Node {
     public enum Number {
         case int(Int)
         case uint(UInt)
         case double(Double)
+        case decimal(Decimal)
     }
 }
 
@@ -29,6 +32,10 @@ extension Node.Number {
     public init(_ value: Double) {
         self = .double(value)
     }
+
+    public init(_ value: Decimal) {
+        self = .decimal(value)
+    }
 }
 
 // MARK: Accessors
@@ -47,6 +54,8 @@ extension Node.Number {
             return Int(u)
         case let .double(d):
             return Int(d)
+        case let .decimal(d):
+            return NSDecimalNumber(decimal: d).intValue
         }
     }
 
@@ -59,6 +68,8 @@ extension Node.Number {
             return u
         case let .double(d):
             return UInt(d)
+        case let .decimal(d):
+            return NSDecimalNumber(decimal: d).uintValue
         }
     }
 
@@ -70,6 +81,21 @@ extension Node.Number {
             return Double(u)
         case let .double(d):
             return Double(d)
+        case let .decimal(d):
+            return NSDecimalNumber(decimal: d).doubleValue
+        }
+    }
+    
+    public var decimal: Decimal {
+        switch self {
+        case let .int(i):
+            return Decimal(i)
+        case let .uint(u):
+            return Decimal(u)
+        case let .double(d):
+            return Decimal(d)
+        case let .decimal(d):
+            return d
         }
     }
 }
@@ -98,6 +124,13 @@ extension Node.Number {
             default:
                 return nil
             }
+        case let .decimal(d):
+            switch d {
+            case 1.0: return true
+            case 0.0: return false
+            default:
+                return nil
+            }
         }
     }
 }
@@ -116,6 +149,9 @@ public func ==(lhs: Node.Number, rhs: Node.Number) -> Bool {
     case let (.int(l), .double(r)):
         guard r.truncatingRemainder(dividingBy: 1) == 0.0 else { return false }
         return l == Int(r)
+    case let (.int(l), .decimal(r)):
+        guard r.exponent >= 0 else { return false }
+        return l == NSDecimalNumber(decimal: r).intValue
     case let (.uint(l), .int(r)):
         guard l <= UInt(Int.max) && r >= 0 else { return false }
         return Int(l) == r
@@ -124,6 +160,9 @@ public func ==(lhs: Node.Number, rhs: Node.Number) -> Bool {
     case let (.uint(l), .double(r)):
         guard r >= 0 && r.truncatingRemainder(dividingBy: 1) == 0.0 else { return false }
         return l == UInt(r)
+    case let (.uint(l), .decimal(r)):
+        guard r >= 0 && r.exponent >= 0 else { return false }
+        return l == NSDecimalNumber(decimal: r).uintValue
     case let (.double(l), .int(r)):
         guard l.truncatingRemainder(dividingBy: 1) == 0.0 else { return false }
         return Int(l) == r
@@ -131,6 +170,18 @@ public func ==(lhs: Node.Number, rhs: Node.Number) -> Bool {
         guard l.truncatingRemainder(dividingBy: 1) == 0.0 else { return false }
         return UInt(l) == r
     case let (.double(l), .double(r)):
+        return l == r
+    case let (.double(l), .decimal(r)):
+        return l == NSDecimalNumber(decimal: r).doubleValue
+    case let (.decimal(l), .int(r)):
+        guard l.exponent >= 0 else { return false }
+        return NSDecimalNumber(decimal: l).intValue == r
+    case let (.decimal(l), .uint(r)):
+        guard l >= 0 && l.exponent >= 0 else { return false }
+        return NSDecimalNumber(decimal: l).uintValue == r
+    case let (.decimal(l), .double(r)):
+        return NSDecimalNumber(decimal: l).doubleValue == r
+    case let (.decimal(l), .decimal(r)):
         return l == r
     }
 }
@@ -159,6 +210,8 @@ extension Node.Number: CustomStringConvertible {
         case let .uint(u):
             return u.description
         case let .double(d):
+            return d.description
+        case let .decimal(d):
             return d.description
         }
     }
