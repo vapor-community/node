695317769ba35e07bdc10ceb96be0035fbe81ed8
Logan Wright <logan.william.wright@gmail.com>
2017-03-01 17:59:47 +0100

    functional

--- Sources/Node/Getters.swift
+++ Sources/Node/Getters.swift
@@ -3,35 +3,35 @@
 extension SchemaWrapper {
     public func get<T, InputType: NodeInitializable>(
         _ indexers: PathIndexer...,
-        transform: (InputType) throws -> T)
-        throws -> T {
-            return try get(path: indexers, transform: transform)
+        transform: (InputType) throws -> T
+    ) throws -> T {
+        return try get(path: indexers, transform: transform)
     }
 
     public func get<T, InputType: NodeInitializable>(
         path indexers: [PathIndexer],
-        transform: (InputType) throws -> T)
-        throws -> T {
-            guard let value = self[indexers] else {
-                throw NodeError(node: nil, expectation: "\(T.self)", indexers: indexers)
-            }
+        transform: (InputType) throws -> T
+    ) throws -> T {
+        guard let value = self[indexers] else {
+            throw NodeError(node: nil, expectation: "\(T.self)", indexers: indexers)
+        }
 
-            let input = try InputType(node: value, in: context)
-            return try transform(input)
+        let input = try InputType(node: value, in: context)
+        return try transform(input)
     }
 
     public func get<T, InputType: NodeInitializable>(
         _ indexers: PathIndexer...,
-        transform: (InputType?) throws -> T)
-        throws -> T {
-            return try get(path: indexers, transform: transform)
+        transform: (InputType?) throws -> T
+    ) throws -> T {
+        return try get(path: indexers, transform: transform)
     }
 
     public func get<T, InputType: NodeInitializable>(
         path indexers: [PathIndexer],
-        transform: (InputType?) throws -> T)
-        throws -> T {
-            return try transform(get(indexers))
+        transform: (InputType?) throws -> T
+    ) throws -> T {
+        return try transform(get(indexers))
     }
 }
 
@@ -51,8 +51,7 @@ extension SchemaWrapper {
                 throw NodeError(node: nil, expectation: "\(T.self)", indexers: indexers)
             }
 
-            let node = Node(value)
-            return try T(node: node)
+            return try T(node: value)
     }
 
     public func get<T : NodeInitializable>(
