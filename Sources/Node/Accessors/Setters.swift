912ebd13ffe8f70b1468f9c7669d52355d32f134
Logan Wright <logan.william.wright@gmail.com>
2017-03-24 10:41:16 +0100

    help prevent security issues by implicitly converting nil to null in setters

--- Sources/Node/Accessors/Setters.swift
+++ Sources/Node/Accessors/Setters.swift
@@ -1,7 +1,7 @@
 extension StructuredDataWrapper {
     public mutating func set(_ path: String, _ value: NodeRepresentable?) throws {
-        let node = try value.flatMap { try $0.makeNode(in: context) }
-        self.wrapped[path] = node?.wrapped
+        let node = try value.flatMap { try $0.makeNode(in: context) } ?? .null
+        self.wrapped[path] = node.wrapped
     }
 }
 
@@ -11,7 +11,21 @@ extension StructuredDataWrapper {
     }
 
     public mutating func set(path indexers: [PathIndexer], to value: NodeRepresentable?) throws {
-        let node = try value.flatMap { try $0.makeNode(in: context) }
-        self.wrapped[indexers] = node?.wrapped
+        let node = try value.flatMap { try $0.makeNode(in: context) } ?? .null
+        self.wrapped[indexers] = node.wrapped
+    }
+}
+
+extension StructuredDataWrapper {
+    public mutating func removeKey(_ path: String) {
+        self.wrapped[path] = nil
+    }
+
+    public mutating func removeKey(_ indexers: PathIndexer...) {
+        self.wrapped[indexers] = nil
+    }
+
+    public mutating func removeKey(_ indexers: [PathIndexer]) {
+        self.wrapped[indexers] = nil
     }
 }
