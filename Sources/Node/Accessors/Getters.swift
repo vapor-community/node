c9d1ca8e5200169b62b443dc7206c3b5dd0b08fd
tanner0101 <me@tanner.xyz>
2017-05-10 23:20:01 +0100

    add non optional array

--- Sources/Node/Accessors/Getters.swift
+++ Sources/Node/Accessors/Getters.swift
@@ -33,6 +33,19 @@ extension StructuredDataWrapper {
 // MARK: Array
 
 extension StructuredDataWrapper {
+    public func get<T: NodeInitializable>(_ indexers: PathIndexer...) throws -> [T] {
+        guard let values = self[indexers]?.array else {
+            throw NodeError.unableToConvert(
+                input: Node(self[indexers]?.wrapped ?? .null),
+                expectation: "Array of \(T.self)",
+                path: indexers
+            )
+        }
+        return try values.map { return try  T(node: $0) }
+    }
+}
+
+extension StructuredDataWrapper {
     public func get<T: NodeInitializable>(_ indexers: PathIndexer...) throws -> [T]? {
         if let values = self[indexers]?.array {
             let nodes = try values.map { return try  T(node: $0) }
@@ -49,7 +62,7 @@ extension StructuredDataWrapper {
     public func get<T, InputType: NodeInitializable>(
         _ indexers: PathIndexer...,
         transform: (InputType) throws -> T
-        ) throws -> T {
+    ) throws -> T {
         return try get(path: indexers, transform: transform)
     }
     
