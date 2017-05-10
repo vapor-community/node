1412994d80db92684a4307e6e3d5863084f34d42
tanner0101 <me@tanner.xyz>
2017-05-10 23:10:57 +0100

    type safe get/set

--- Sources/Node/Accessors/Getters.swift
+++ Sources/Node/Accessors/Getters.swift
@@ -1,19 +1,45 @@
 extension StructuredDataWrapper {
     public func get<T : NodeInitializable>(
-        _ indexers: PathIndexer...)
-        throws -> T {
-            return try get(indexers)
+        _ indexers: PathIndexer...
+        ) throws -> T {
+        return try get(indexers)
     }
-
+    
     public func get<T : NodeInitializable>(
-        _ indexers: [PathIndexer])
-        throws -> T {
-            do {
-                let value = self[indexers] ?? .null
-                return try T(node: value)
-            } catch let error as NodeError {
-                throw error.appendPath(indexers)
-            }
+        _ indexers: [PathIndexer]
+        ) throws -> T {
+        do {
+            let value = self[indexers] ?? .null
+            return try T(node: value)
+        } catch let error as NodeError {
+            throw error.appendPath(indexers)
+        }
+    }
+}
+
+// MARK: Optional
+
+extension StructuredDataWrapper {
+    public func get<T: NodeInitializable>(_ indexers: PathIndexer...) throws -> T? {
+        if let value = self[indexers], value.wrapped != .null {
+            let item = try T(node: value)
+            return .some(item)
+        } else {
+            return .none
+        }
+    }
+}
+
+// MARK: Array
+
+extension StructuredDataWrapper {
+    public func get<T: NodeInitializable>(_ indexers: PathIndexer...) throws -> [T]? {
+        if let values = self[indexers]?.array {
+            let nodes = try values.map { return try  T(node: $0) }
+            return .some(nodes)
+        } else {
+            return .none
+        }
     }
 }
 
@@ -23,17 +49,16 @@ extension StructuredDataWrapper {
     public func get<T, InputType: NodeInitializable>(
         _ indexers: PathIndexer...,
         transform: (InputType) throws -> T
-    ) throws -> T {
+        ) throws -> T {
         return try get(path: indexers, transform: transform)
     }
-
+    
     public func get<T, InputType: NodeInitializable>(
         path indexers: [PathIndexer],
         transform: (InputType) throws -> T
-    ) throws -> T {
+        ) throws -> T {
         let value = self[indexers] ?? .null
         let input = try InputType(node: value, in: context)
         return try transform(input)
     }
 }
-
