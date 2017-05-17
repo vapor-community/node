400fb8fa8359e42c6fea82d03e9acfaa6799d60e
Logan Wright <logan.william.wright@gmail.com>
2017-05-17 14:57:11 +0100

    add id

---
+++ Sources/Node/Utilities/Identifier.swift
@@ -0,0 +1,14 @@
+/// Represents a convenience around various identifier types.
+public struct Identifier: StructuredDataWrapper {
+    public var wrapped: StructuredData
+    public let context: Context
+
+    public init(_ wrapped: StructuredData, in context: Context?) {
+        self.wrapped = wrapped
+        self.context = context ?? emptyContext
+    }
+
+    public init() {
+        self.init([:])
+    }
+}
