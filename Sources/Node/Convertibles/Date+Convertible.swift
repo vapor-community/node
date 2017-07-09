d84d20bb82027fe8a8fdd7e29842330f0079cc19
Larry Damman <ldamman@users.noreply.github.com>
2017-07-09 18:49:27 -0400

    Update Date+Convertible.swift

--- Sources/Node/Convertibles/Date+Convertible.swift
+++ Sources/Node/Convertibles/Date+Convertible.swift
@@ -103,7 +103,7 @@ extension DateFormatter {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
         return formatter
     }()
 }
