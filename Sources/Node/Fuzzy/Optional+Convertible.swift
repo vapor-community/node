2793d425c5b3ce8d81b6011b02048187575f0c8f
Zsolt Váradi <vzsg@users.noreply.github.com>
2017-12-26 11:04:19 +0100

    Add NodeConvertible conformance to IUOs

--- Sources/Node/Fuzzy/Optional+Convertible.swift
+++ Sources/Node/Fuzzy/Optional+Convertible.swift
@@ -18,3 +18,25 @@ extension Optional: NodeConvertible {
         }
     }
 }
+
+extension ImplicitlyUnwrappedOptional: NodeConvertible {
+    public init(node: Node) throws {
+        guard node != .null else {
+            self = .none
+            return
+        }
+
+        let wrapped: Wrapped = try Node.fuzzy.initialize(node: node)
+        self = .some(wrapped)
+    }
+
+    public func makeNode(in context: Context?) throws -> Node {
+        switch self {
+        case .none:
+            return .null
+        case .some(let some):
+            return try Node.fuzzy.represent(some, in: context)
+        }
+    }
+}
+
