be448120b3b0284014d7961957153b9606ea99f0
Zsolt Váradi <vzsg@users.noreply.github.com>
2017-12-30 02:09:46 +0100

    Conform StructuredData(Wrapper) to Encodable

--- Sources/Node/StructuredDataWrapper/StructuredDataWrapper.swift
+++ Sources/Node/StructuredDataWrapper/StructuredDataWrapper.swift
@@ -1,3 +1,34 @@
+#if swift(>=4.0)
+import Foundation
+
+public protocol StructuredDataWrapper:
+    NodeConvertible,
+    PathIndexable,
+    Equatable,
+    ExpressibleByNilLiteral,
+    ExpressibleByBooleanLiteral,
+    ExpressibleByIntegerLiteral,
+    ExpressibleByFloatLiteral,
+    ExpressibleByStringLiteral,
+    ExpressibleByArrayLiteral,
+    ExpressibleByDictionaryLiteral,
+    Encodable
+{
+    static var defaultContext: Context? { get }
+
+    var wrapped: StructuredData { get set }
+    var context: Context { get }
+    init(_ wrapped: StructuredData, in context: Context?)
+}
+
+extension StructuredDataWrapper { // : Encodable
+    public func encode(to encoder: Encoder) throws {
+        try wrapped.encode(to: encoder)
+    }
+}
+
+#else
+
 public protocol StructuredDataWrapper:
     NodeConvertible,
     PathIndexable,
@@ -16,6 +47,7 @@ public protocol StructuredDataWrapper:
     var context: Context { get }
     init(_ wrapped: StructuredData, in context: Context?)
 }
+#endif
 
 extension StructuredDataWrapper {
     public static var defaultContext: Context? { return nil }
